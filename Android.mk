c2cfb8b57633ec983892bf1092e2c76bf8b8855d
YC Shen <fred.shen@mediatek.com>
2023-01-02 21:33:28 +0800

    [ALPS05675388] Wifi: Support Virtual Build
    [Detail]
    Rename layer decoupling 2.0 variable to avoid conflict.
    
    MTK-Commit-Id: a82fb5619a6aeeeff0436e830b26a660e5f9e3de
    
    Change-Id: Ic3f3e4e907cf065d36b052452b12d06240213ebc
    CR-Id: ALPS05675388
    Feature: [Module]Wi-Fi Driver
    Signed-off-by: YC Shen <fred.shen@mediatek.com>

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(MTK_WLAN_SUPPORT), yes)
 
-ifeq ($(WLAN_CHIP_ID), common)
+ifeq ($(WLAN_BUILD_COMMON), true)
 	# for layer decoupling 2.0, we have to build all configurations
 	CONNAC_VER := 2_0
 	WIFI_NAME := wmt_chrdev_wifi_connac2
